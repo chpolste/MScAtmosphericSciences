Fixed sign error in line shape. Numerically stabilized model.
In the current state I have no formulation for the Jacobian of the
forward model... Additionally there are some severe problems with the FAP.

software/mwrt/fap.py
@@ -36,9 +36,9 @@ def partition_lnq(p, T, lnq):
 def predictor_data(n):
     """Generate training data for FAP training."""
     from sklearn.utils.extmath import cartesian
-    ps = np.linspace(50, 980, n)
-    Ts = np.linspace(170, 320, n)
-    ss = np.linspace(0.001, 1.3, n)
+    ps = np.linspace(80, 1000, n)
+    Ts = np.linspace(170, 330, n)
+    ss = np.linspace(0.0001, 1.3, n)
     data = cartesian([ps, Ts, ss])
     # Remove some unrealistic data
     remove = (
@@ -74,8 +74,8 @@ class FastAbsorptionPredictor:
     def fit(self, predictors):
         """"""
         self.polyfeatures.fit(predictors)
-        target = self.model(p=predictors[:,0], T=predictors[:,1],
-                lnq=predictors[:,2])
+        target = np.log10(self.model(p=predictors[:,0], T=predictors[:,1],
+                lnq=predictors[:,2]))
         self.regression.fit(self.polyfeatures.transform(predictors), target)
 
     def generate_code(self):
@@ -85,23 +85,23 @@ class FastAbsorptionPredictor:
         powers = self.polyfeatures.powers_
         method_indent = " "*16
         out.append("class {}:\n".format(self.name))
-        out.append('"""Fast Absorption Predictor."""\n\n')
+        out.append('    """Fast Absorption Predictor."""\n\n')
         out.append("    @staticmethod\n")
         out.append("    def forward(p, T, lnq):\n")
-        out.append("        return (\n")
+        out.append("        return 10**(\n")
         out.append(method_indent)
         out.append(method_indent.join(self._generate_terms(coeffs, powers)))
         out.append("                )\n\n")
         out.append("    @staticmethod\n")
         out.append("    def jacobian_T(p, T, lnq):\n")
-        out.append("        return (\n")
+        out.append("        return np.diag(\n")
         out.append(method_indent)
         out.append(method_indent.join(self._generate_terms(
                 *self._derive(coeffs, powers, 1))))
         out.append("                )\n\n")
         out.append("    @staticmethod\n")
         out.append("    def jacobian_lnq(p, T, lnq):\n")
-        out.append("        return (\n")
+        out.append("        return np.diag(\n")
         out.append(method_indent)
         out.append(method_indent.join(self._generate_terms(
                 *self._derive(coeffs, powers, 2))))

software/mwrt/model.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 
 import numpy as np
 from scipy.integrate import trapz, cumtrapz
+from scipy.interpolate import interp1d
 
 
 # Cache type
@@ -30,10 +31,15 @@ class MWRTM:
         assert "p" in data or p is not None
         assert "T" in data or T is not None
         assert "lnq" in data or lnq is not None
-        self.z = np.array(data["z"]) if "z" in data else np.array(z)
-        self.p = np.array(data["p"]) if "p" in data else np.array(p)
-        self.T = np.array(data["T"]) if "T" in data else np.array(T)
-        self.lnq = np.array(data["lnq"]) if "lnq" in data else np.array(lnq)
+        z = np.array(data["z"]) if "z" in data else np.array(z)
+        p = np.array(data["p"]) if "p" in data else np.array(p)
+        T = np.array(data["T"]) if "T" in data else np.array(T)
+        lnq = np.array(data["lnq"]) if "lnq" in data else np.array(lnq)
+        # Interpolate
+        self.z = np.linspace(z[0], z[-1], 100000)
+        self.p = interp1d(z, p)(self.z)
+        self.T = interp1d(z, T)(self.z)
+        self.lnq = interp1d(z, lnq)(self.z)
         # Absorption prediction
         self.fap = fap
         # Setup cache
@@ -57,16 +63,16 @@ class MWRTM:
         """Calculate absorption coefficients with FAP and store in cache."""
         self.α = Cached(
                 self.fap.forward(self.p, self.T, self.lnq),
-                self.fap.jacobian_T(self.p, self.T, self.lnq),
-                self.fap.jacobian_lnq(self.p, self.T, self.lnq)
+                None, #self.fap.jacobian_T(self.p, self.T, self.lnq),
+                None #self.fap.jacobian_lnq(self.p, self.T, self.lnq)
                 )
 
     def _cache_optical_depth(self):
         """"""
         self.τ = Cached(
                 -cumtrapz(self.α.fwd, self.z, initial=0),
-                -cumtrapz(self.α.jT, self.z, axis=1, initial=0),
-                -cumtrapz(self.α.jlnq, self.z, axis=1, initial=0)
+                None, #-cumtrapz(self.α.jT, self.z, axis=1, initial=0),
+                None, #-cumtrapz(self.α.jlnq, self.z, axis=1, initial=0)
                 )
 
 

software/mwrt/refractivity_l93.py
@@ -28,7 +28,7 @@ def van_vleck_weisskopf(νeval, ν, γ, δ):
     """
     term1 = (1. - 1j*δ)/(ν - νeval - 1j*γ)
     term2 = (1. + 1j*δ)/(ν + νeval + 1j*γ)
-    return νeval * (term1 + term2)
+    return νeval * (term1 - term2)
 
 
 # Dry-air module (2.2)
@@ -59,9 +59,11 @@ def refractivity_O2_lines(ν, θ, pd, e):
     N = 0.
     for line in O2_lines:
         # Line strength
+        #S = line.a1 / line.ν * pd * θ**3 * np.exp(line.a2*(1-θ))
         S = line.a1 / line.ν * pd * θ**3 * np.exp(line.a2*(1-θ))
         # Width parameter (no mesosphere consideration)
         γ = line.a3 * (pd * θ**line.a4 + 1.10 * e * θ)
+        γ = np.sqrt(γ*γ + (25*0.6e-4)**2)
         # Overlap parameter
         δ = (line.a5 + line.a6 * θ) * (pd+e) * θ**0.8
         # Refractivity