Fixed a critical sign error in optimal estimation procedure.

software/optimal_estimation.py
@@ -94,7 +94,7 @@ class OptimalEstimationRetrieval:
         μ = self.μs[-1]
         Fμ, jac = self.model(μ, self.p0)
         rhs = (jac.T @ self.obserr.covi @ (self.y - Fμ - self.obserr.mean)
-                + self.prior.covi @ (μ - self.prior.mean))
+                - self.prior.covi @ (μ - self.prior.mean))
         covi = self.prior.covi + jac.T @ self.obserr.covi @ jac
         lhs = covi + γ * self.prior.covi
         diff = np.linalg.solve(lhs, rhs)

software/run_optest.py
@@ -27,10 +27,10 @@ if __name__ == "__main__":
     _, obs_cov_v = split_bands(obs_cov)
     obs_cov_z = get_zenith(obs_cov)
 
-    # Add 0.5 K uncorrelated instrument noise
-    obs_cov = obs_cov + 0.5*np.eye(obs_cov.shape[0])
-    obs_cov_v = obs_cov_v + 0.5*np.eye(obs_cov_v.shape[0])
-    obs_cov_z = obs_cov_z + 0.5*np.eye(obs_cov_z.shape[0])
+    # Add 0.5 K uncorrelated instrument noise (0.5 K std = 0.25 K² cov)
+    obs_cov = obs_cov + 0.25*np.eye(obs_cov.shape[0])
+    obs_cov_v = obs_cov_v + 0.25*np.eye(obs_cov_v.shape[0])
+    obs_cov_z = obs_cov_z + 0.25*np.eye(obs_cov_z.shape[0])
 
     obs_error = Gaussian(np.zeros(obs_cov.shape[0]), obs_cov)
     obs_error_v = Gaussian(np.zeros(obs_cov_v.shape[0]), obs_cov_v)
@@ -100,7 +100,7 @@ if __name__ == "__main__":
 
         i += 1
 
-        #if i > 5: break
+        #if i > 20: break
 
     valids = pd.Series(valids, name="valid")
     pd.DataFrame(np.vstack([convergeds, iterations]).T, columns=["converged", "iterations"],